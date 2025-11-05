c8239dd8bcc74c4aeda5234d476727d07bd8bbcf
datafixer <datafixer.br@gmail.com>
2025-11-05 16:32:58 -0300

    adiciona o módulo de erros customizados

--- src/utils/exceptions.py
+++ src/utils/exceptions.py
@@ -1,9 +1,51 @@
 """
-Módulo de exceções customizadas do projeto
+Exceções customizadas do projeto.
 """
 
 
-class ZipProcessingError(BaseException):
-    """Error no processamento do arquivo ZIP"""
+class CNPQProcessingError(Exception):
+    """Exceção base para erros de processamento."""
+
+    pass
+
+
+class ConfigurationError(CNPQProcessingError):
+    """Erro de configuração."""
+
+    pass
+
+
+class ZipProcessingError(CNPQProcessingError):
+    """Erro no processamento de arquivos ZIP."""
+
+    pass
+
+
+class FileNotFoundError(CNPQProcessingError):
+    """Arquivo não encontrado."""
+
+    pass
+
+
+class OCIConnectionError(CNPQProcessingError):
+    """Erro de conexão com OCI."""
+
+    pass
+
+
+class OCIUploadError(CNPQProcessingError):
+    """Erro no upload para OCI."""
+
+    pass
+
+
+class MetadataError(CNPQProcessingError):
+    """Erro no processamento de metadados."""
+
+    pass
+
+
+class BatchProcessingError(CNPQProcessingError):
+    """Erro no processamento em lote."""
 
     pass
